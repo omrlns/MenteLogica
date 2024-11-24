9477ad6584a1f117816fd116f5684526741ce005
Marlon <marlon.da.silva996@gmail.com>
2024-11-23 23:02:45 -0300

    preparando um café?

--- dia1.py
+++ dia1.py
@@ -1,1 +1,15 @@
 print('bem-vindo ao método mente lógica, Marlon.')
+
+# EXERCÍCIOS PRÁTICOS
+# 1, reflexão sobre lógica (preparando um café)
+
+# pegue a chaleira e coloque água (é importante lembrar que a quantidade pode variar de pessoa para pessoa)
+# coloque a água pra esquentar
+# prepare o coador com o filtro de papel
+# abra a garrafa e coloque o coador com o filtro em cima da garrafa, de um jeito que o líquido possa cair dentro da garrafa
+# coloque o pó de café dentro do coador de papel (a mesma situação da água se aplica ao pó, a quantidade vai variar de pessoa para pessoa)
+# quando a água já estiver quente, vire ela de forma moderada dentro do coador
+# repita o processo de virar a água dentro do coador até a água acabar ou até a garrafa encher
+# retire o coador
+# feche a garrafa
+
