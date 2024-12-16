8f9f5105b67667c967cc0dbf803e380e80c91a93
Marlon <marlon.da.silva996@gmail.com>
2024-12-15 22:35:58 -0300

    exercícios práticos

--- dia4.py
+++ dia4.py
@@ -58,3 +58,101 @@ else:
 
 print(linha)
 
+# EXERCÍCIOS PRÁTICOS
+# 1, verificando se um número é positovo, negativo ou zero
+numero = float(input('digite um número: '))
+
+if numero > 0:
+    print('o número é positivo.')
+elif numero < 0:
+    print('o número é negativo.')
+else:
+    print('o número é zero.')
+
+print(linha)
+
+# 2, calculadora simples
+numero1 = float(input('digite o primeiro número: '))
+numero2 = float(input('digite o segundo número: '))
+operacao = input('digite a operação (+, -, *, /): ')
+
+if operacao == '+':
+        resultado = numero1 + numero2
+        print('resultado:', resultado)
+elif operacao == '-':
+        resultado = numero1 - numero2
+        print('resultado:', resultado)
+elif operacao == '*':
+        resultado = numero1 * numero2
+        print('resultado:', resultado)
+elif operacao == '/':
+    if numero2 != 0:
+        resultado = numero1 / numero2
+        print('resultado:', resultado)
+    else:
+        print('erro: divisão por zero!')
+else:
+     print('operação inválida.')
+
+print(linha)
+
+# 3, classificação de idade
+idade = int(input('digite a sua idade: '))
+
+if idade >= 0 and idade <= 12:
+     print("você é uma criança.")
+elif idade >= 13 and idade <= 17:
+     print("você é um adolescente.")
+elif idade >= 18 and idade <= 59:
+     print('você é um adulto.')
+elif idade >= 60:
+     print('você é um idoso.')
+else:
+     print('idade inválida.')
+
+print(linha)
+
+# 4, verificando ano bissexto
+ano = int(input('digite um ano: '))
+
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 ==0):
+     print(ano, 'é um ano bissexto.')
+else:
+     print(ano, 'não é um ano bissexto.')
+
+print(linha)
+
+# 5, simulador de caixa eletrônico
+valor_saque = int(input("digite o valor do saque: R$"))
+if valor_saque <= 0:
+    print("valor inválido.")
+else:
+    cedulas_100 = valor_saque // 100
+    valor_saque %= 100 #atualiza o valor restante do módulo para as outros notas
+    cedulas_50 = valor_saque // 50
+    valor_saque %= 50
+    cedulas_20 = valor_saque // 20
+    valor_saque %= 20
+    cedulas_10 = valor_saque // 10
+    valor_saque %= 10
+    cedulas_5 = valor_saque // 5
+    valor_saque %= 5
+    cedulas_2 = valor_saque // 2
+    valor_saque %= 2
+    if valor_saque != 0:
+        print("não é possível sacar o valor especificado com as cédulas disponíveis.")
+    else:
+        print('cédulas entregues:')
+        if cedulas_100 > 0:
+            print(f"{cedulas_100} x R$100")
+        if cedulas_50 > 0:
+            print(f"{cedulas_50} x R$50")
+        if cedulas_20 > 0:
+            print(f"{cedulas_20} x R$20")
+        if cedulas_10 > 0:
+            print(f"{cedulas_10} x R$10")
+        if cedulas_5 > 0:
+            print(f"{cedulas_5} x R$5")
+        if cedulas_2 > 0:
+            print(f"{cedulas_2} x R$2")
+
