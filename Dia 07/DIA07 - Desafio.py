e1461c5b4f230a70122ee87732377b4a0f5603de
omrlns <marlon.da.silva996@gmail.com>
2025-07-31 14:57:30 -0300

    remodulação inicial do jogo de adivinhação

--- Dia 07/DIA07 - Desafio.py
+++ Dia 07/DIA07 - Desafio.py
@@ -9,52 +9,18 @@
 from random import randint
 
 # lista para armazenar as pontuações
-pontuacoes = []
+pontuacao = []
+tentativasRestantes = 10
 
-print('=' * 22)
-print(' < GUESS THE NUMBER > ')
-print('=' * 22)
-
-while True:
-    numeroCPU = randint(1, 100)
-    tentativasRestantes = 5
-    tentativas = 0
-
-    print('EU PENSEI EM UM NÚMERO ENTRE 1 E 100...')
-    print('VOCÊ CONSEGUE ADIVINHAR?')
-    print('USE AS SUAS 7 TENTATIVAS SABIAMENTE!')
-
-
-    while tentativasRestantes > 0:
-        palpite = int(input('\nDIGITE O SEU PALPITE: '))
-
-        tentativas += 1
-        tentativasRestantes -= 1
-
-        if (palpite == numeroCPU):
-            print('PARABÉNS, VOCÊ ACERTOU!')
-            print('VOCÊ ACERTOU O NÚMERO EM {} TENTATIVA(S)!'.format(tentativas))
-            pontuacao = tentativasRestantes * 10
-            pontuacoes.append(pontuacao)
-            print('\nSUA PONTUAÇÃO NESTA PARTIDA: {} PONTOS!'.format(pontuacao))
-            break
-        elif (palpite < numeroCPU):
-            print('\nO NÚMERO SECRETO É MAIOR QUE O SEU PALPITE!')
-        else:
-            print('\nO NÚMERO SECRETO É MENOR QUE O SEU PALPITE!')
-
-        print('\nTENTATIVAS RESTANTES: {}'.format(tentativasRestantes))
-
-    else:
-        print('\nQUE PENA, VOCÊ PERDEU! \nO NÚMERO SECRETO ERA: {}'.format(numeroCPU))
-        pontuacoes.append(0)
-
-    print('\n < PLACAR > ')
-    for i, pontos in enumerate(pontuacoes, start=1):
-        print('\nPARTIDA #{}: {} PONTO(S).'.format(i, pontos))
-
-    continuar = input('\nVOCÊ DESEJA JOGAR NOVAMENTE? [S/N] ').strip().upper()[0]
-    if (continuar != 'S'):
-        print('OBRIGADO POR JOGAR! ATÉ A PRÓXIMA!')
+print('CPU: EU PENSEI EM UM NÚMERO DE 1 A 100, VOCÊ CONSEGUE ADIVINHAR?')
+while tentativasRestantes >= 1:
+    numeroCPU = randint(1, 10)
+    palpite = int(input('INFORME O SEU PALPITE: '))
+    if (palpite < 1 or palpite > 100):
+        print('ERRO! DIGITE UM NÚMERO DE 1 A 100.')
+    if (palpite == numeroCPU):
+        print('VOCÊ ACERTOU!')
+        pontuacao.append(tentativasRestantes * 5)
         break
-
+print()
+print('PONTUAÇÃO: {} ponto(s)!'.format(pontuacao))
